2d8fc87bd58b1f16a0cfaa286cfa6bc6c514d315
DeylerAF <deylerafernandez@outlook.com>
2023-10-22 11:31:08 -0600

    New Portfolio using React and Tailwind

--- README.md
+++
@@ -1,2 +0,0 @@
-# DeylerAF
-This is my website portfolio.
--- script.js
+++
@@ -1,5 +0,0 @@
-import "./js/darkMode.js";
-import "./js/menu.js";
-import "./js/typewriter.js";
-import "./js/swiper.js";
-import "./js/scrollUp.js";
